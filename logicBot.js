2f4b72c2ccf7e87b5ece478d0e4a22c19a2ffe65
BEPZ22 <brauliopicon96@gmail.com>
2019-02-04 14:43:46 -0400

    logicBot y Gitignore actulizados
    Archivos actualizados del gitignore y la logica del bot

---
+++ logicBot.js
@@ -0,0 +1,14 @@
+const Telegraf = require('telegraf');
+
+const API_TOKEN = process.env.BOT_TOKEN || ''
+const PORT = process.env.PORT || 3000
+const URL = process.env.BOT_URL
+
+const bot = new Telegraf(API_TOKEN);
+bot.telegram.setWebhook(`${URL}bot${API_TOKEN}`);
+bot.startWebhook(`/bot${API_TOKEN}`, null, PORT)
+
+bot.start((context)=>{
+    console.log('synergyvisionbot started', context.from.id)
+    return context.reply('El Bot de Synergy Vision te da la bienvenida.')
+  })
