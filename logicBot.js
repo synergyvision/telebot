9980fff8802d91ee3ae2aa625729c8934a45be97
BEPZ22 <brauliopicon96@gmail.com>
2019-02-05 12:27:13 -0400

    Update logicBot.js

--- logicBot.js
+++ logicBot.js
@@ -4,23 +4,13 @@ const express = require('express');
 const API_TOKEN = process.env.BOT_TOKEN || '' ;
 const PORT = process.env.PORT || 3000 ;
 const URL = process.env.BOT_URL ;
-const URL_BOT = process.env.BOT_URL_SECRET;
 
 const expressApp = express();
 const bot = new Telegraf(API_TOKEN);
 
-expressApp.use(bot.webhookCallback(`/secret-path`));
 bot.telegram.setWebhook(`${URL} bot${API_TOKEN}`);
 
 bot.startWebhook(`/bot${API_TOKEN}`, null, PORT);
-
-expressApp.get('/', (req, res) => {
-  res.send('Hello World!')
-});
-
-expressApp.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`)
-});
 
 bot.start((context)=>{
     console.log('synergyvisionbot started', context.from.id)
