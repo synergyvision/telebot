ed8892578a2304879082fac59aebd1cba07219fc
BEPZ22 <brauliopicon96@gmail.com>
2019-02-05 17:05:21 -0400

    Update logicBot.js

--- logicBot.js
+++ logicBot.js
@@ -7,9 +7,9 @@ const URL = process.env.BOT_URL ;
 const bot = new Telegraf(API_TOKEN);
 
 
-bot.telegram.setWebhook(`${URL} bot${API_TOKEN}`);
-
+bot.telegram.setWebhook(`${URL}bot${API_TOKEN}`);
 bot.startWebhook(`/bot${API_TOKEN}`, null, PORT);
+
 
 bot.start((context)=>{
     console.log('synergyvisionbot started', context.from.id)
