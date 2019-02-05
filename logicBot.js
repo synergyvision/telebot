a5bec5fd50d0154f62c7e355da425f920e844aef
BEPZ22 <brauliopicon96@gmail.com>
2019-02-05 10:07:55 -0400

    Update logicBot.js

--- logicBot.js
+++ logicBot.js
@@ -1,11 +1,18 @@
 const Telegraf = require('telegraf');
+const express = require('express') 
+const expressApp = express()
 
 const API_TOKEN = process.env.BOT_TOKEN || ''
 const PORT = process.env.PORT || 3000
-const URL = process.env.BOT_URL
 
+expressApp.get('/', (req, res) => { res.send('Hello World!') }) 
+expressApp.listen(port, () => { console.log(`Listening on port ${port}`) })
+
+const URL = process.env.BOT_URL 
 const bot = new Telegraf(API_TOKEN);
-bot.telegram.setWebhook(`${URL}bot${API_TOKEN}`);
+
+
+bot.telegram.setWebhook(`${URL}/bot${API_TOKEN}`);
 bot.startWebhook(`/bot${API_TOKEN}`, null, PORT)
 
 bot.start((context)=>{
