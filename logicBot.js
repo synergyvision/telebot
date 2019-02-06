2170820822b0f94e76aa8c49d56b11f75905b9e2
BEPZ22 <brauliopicon96@gmail.com>
2019-02-06 14:08:05 -0400

    logicBot
    Probando Botones

--- logicBot.js
+++ logicBot.js
@@ -1,4 +1,6 @@
 const Telegraf = require('telegraf');
+const Markup = require('telegraf/markup');
+//const command = require('.comandBot');
 
 const API_TOKEN = process.env.BOT_TOKEN || '';
 const PORT = process.env.PORT || 3000;
@@ -16,4 +18,16 @@ bot.start((context)=>{
 bot.command('ayuda', (context) => {
     context.reply('Esta es la ayuda!!')
   })
-  
+
+  bot.command('Informacion', (context) => {
+    let botoms = [
+        Markup.callbackButton('Información General', 'info'),
+        Markup.callbackButton('Servicios', 'service'),
+        Markup.callbackButton('Misión', 'mision'),
+        Markup.callbackButton('Visión', 'vision'),
+        Markup.callbackButton('Unetenos', 'joinus'),
+        Markup.callbackButton('Visitanos', 'visitus')
+    ]
+    let message = Markup.inlineKeyboard(botoms).extra();
+    bot.telegram.sendMessage(context.from.id,'Conocenos', message)
+  })
