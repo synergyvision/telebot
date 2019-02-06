cbf0144a5ead0ea909447e6bd237e03ea74cbfe5
BEPZ22 <brauliopicon96@gmail.com>
2019-02-06 11:24:25 -0400

    Update logicBot.js

--- logicBot.js
+++ logicBot.js
@@ -1,18 +1,19 @@
 const Telegraf = require('telegraf');
 
-const API_TOKEN = process.env.BOT_TOKEN || ''
-const PORT = process.env.PORT || 3000
-const URL = process.env.BOT_URL
+const API_TOKEN = process.env.BOT_TOKEN || '';
+const PORT = process.env.PORT || 3000;
+const URL = process.env.BOT_URL;
 
 const bot = new Telegraf(API_TOKEN);
 bot.telegram.setWebhook(`${URL}bot${API_TOKEN}`);
-bot.startWebhook(`/bot${API_TOKEN}`, null, PORT)
+bot.startWebhook(`/bot${API_TOKEN}`, null, PORT);
 
 bot.start((context)=>{
   console.log('synergyvisionbot started', context.from.id)
   return context.reply('Bienvenidos a Synergy Vision.')
-})
+});
 
 bot.command('ayuda', (context) => {
     context.reply('Esta es la ayuda!!')
   })
+  
