9fe94e3a9dfebc108fced33079b8f14a1fc61402
BEPZ22 <brauliopicon96@gmail.com>
2019-02-07 18:52:27 -0400

    Creando variables de entorno
    Variables de entorno para configuracion con la base de datos

--- firebaseBot.js
+++ firebaseBot.js
@@ -1,10 +1,24 @@
 var admin = require("firebase-admin");
+
 const URL_DB = process.env.BD_DATABASEURL;
-var serviceAccount = require('./visiontelebotjson.json');
+
+var serviceAccount = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECTID,
+  private_key_id: process.env.FIREBASE_PRIVATEKEYID,
+  private_key: process.env.FIREBASE_PRIVATEKEY,
+  client_email: process.env.FIREBASE_CLIENTEMAIL,
+  client_id: process.env.FIREBASE_CLIENTID,
+  auth_uri: process.env.FIREBASE_AUTHURI,
+  token_uri: process.env.FIREBASE_TOKENURI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTHPROVIDER,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT
+
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: URL_DB
+  databaseURL: process.env.FIREBASE_DATABASEURL
 });
 
   
