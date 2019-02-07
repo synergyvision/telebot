3dbfa3486d1972527c52b0062f49db871e6297b6
BEPZ22 <brauliopicon96@gmail.com>
2019-02-07 15:11:42 -0400

    Requiriendo comando
    Se necesitara de los comandos para que el usuario a traves de telegram interactue con la base de datos

--- logicBot.js
+++ logicBot.js
@@ -1,6 +1,6 @@
 const Telegraf = require('telegraf');
 const Markup = require('telegraf/markup');
-//const command = require('.comandBot');
+const command = require('./comandBot');
 
 const API_TOKEN = process.env.BOT_TOKEN || '';
 const PORT = process.env.PORT || 3000;
@@ -9,6 +9,7 @@ const URL = process.env.BOT_URL;
 const bot = new Telegraf(API_TOKEN);
 bot.telegram.setWebhook(`${URL}bot${API_TOKEN}`);
 bot.startWebhook(`/bot${API_TOKEN}`, null, PORT);
+
 
 bot.start((context)=>{
   console.log('synergyvisionbot started', context.from.id);
