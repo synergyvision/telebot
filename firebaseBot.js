54c86cd00369132f30ed8071095a0a21a77bf10a
BEPZ22 <brauliopicon96@gmail.com>
2019-02-07 17:37:22 -0400

    cambios

--- firebaseBot.js
+++ firebaseBot.js
@@ -1,10 +1,10 @@
 var admin = require("firebase-admin");
-
+const URL_DB = process.env.BD_DATABASEURL;
 var serviceAccount = require('./visiontelebotjson.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: ""
+  databaseURL: URL_DB
 });
 
   
