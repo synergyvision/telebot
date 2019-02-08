c63201127c0902177a7721311f9d1daa220f55e1
BEPZ22 <brauliopicon96@gmail.com>
2019-02-08 10:30:50 -0400

    Update firebaseBot.js

--- firebaseBot.js
+++ firebaseBot.js
@@ -5,7 +5,7 @@ var serviceAccount = {
   type: process.env.FIREBASE_TYPE,
   project_id: process.env.FIREBASE_PROJECTID,
   private_key_id: process.env.FIREBASE_PRIVATEKEYID,
-  private_key: process.env.FIREBASE_PRIVATEKEY,
+  private_key: JSON.parse(process.env.FIREBASE_PRIVATEKEY),
   client_email: process.env.FIREBASE_CLIENTEMAIL,
   client_id: process.env.FIREBASE_CLIENTID,
   auth_uri: process.env.FIREBASE_AUTHURI,
