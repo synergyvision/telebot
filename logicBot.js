a81be5a8732f8b27b6a3ab6ae35f2b4f16e0adc7
Braulio <brauliopicon96@gmail.com>
2019-02-07 22:33:00 -0400

    configuracion

--- logicBot.js
+++ logicBot.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 const URL = process.env.BOT_URL;
 
 const bot = new Telegraf(API_TOKEN);
-bot.telegram.setWebhook(`${URL}/bot${API_TOKEN}`);
+bot.telegram.setWebhook(`${URL}bot${API_TOKEN}`);
 bot.startWebhook(`/bot${API_TOKEN}`, null, PORT);
 
 
