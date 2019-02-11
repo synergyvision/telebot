276844796a8634b1e2bcbf2a965ac77a94f42737
BEPZ22 <brauliopicon96@gmail.com>
2019-02-11 16:47:20 -0400

    Cambios

--- firebaseBot.js
+++ firebaseBot.js
@@ -5,7 +5,7 @@ var serviceAccount = {
   type: process.env.FIREBASE_TYPE,
   project_id: process.env.FIREBASE_PROJECTID,
   private_key_id: process.env.FIREBASE_PRIVATEKEYID,
-  private_key: process.env.FIREBASE_PRIVATEKEY,
+  private_key: process.env.FIREBASE_PRIVATEKEY.replace(/\\n/g, '\n'),
   client_email: process.env.FIREBASE_CLIENTEMAIL,
   client_id: process.env.FIREBASE_CLIENTID,
   auth_uri: process.env.FIREBASE_AUTHURI,
