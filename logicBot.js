5f0b0d9b66a8d331e9065dd5486f475c9cc277f2
BEPZ22 <brauliopicon96@gmail.com>
2019-02-11 09:07:22 -0400

    Delegacion de Funcionalidades
    Separando responsabilidades entre "clases"

--- logicBot.js
+++ logicBot.js
@@ -6,67 +6,11 @@ const PORT = process.env.PORT || 3000;
 const URL = process.env.BOT_URL;
 
 const bot = new Telegraf(API_TOKEN);
+
 bot.telegram.setWebhook(`${URL}bot${API_TOKEN}`);
 bot.startWebhook(`/bot${API_TOKEN}`, null, PORT);
 
 
-bot.start((context)=>{
-  console.log('synergyvisionbot started', context.from.id);
-  return context.reply('Bienvenidos a Synergy Vision.')
-});
 
-bot.hears('Alo', (context) => {
-return context.reply('Si estamos funcionando')
-});
-
-bot.hears('Informacion', (context) => {
-    let botoms = [
-        botoms2 = [Markup.callbackButton('Información General', 'info'),
-        Markup.callbackButton('Servicios', 'service')],
-
-        botoms3 = [Markup.callbackButton('Misión', 'mision'),
-        Markup.callbackButton('Visión', 'vision')],
-
-        botom4 = [Markup.callbackButton('Unetenos', 'joinus'),
-        Markup.callbackButton('Visitanos', 'visitus')]
-    ]
-
-    let message = Markup.inlineKeyboard(botoms).extra()
-    
-    message['parse_mode'] = 'HTML'
-
-    bot.telegram.sendMessage(context.from.id,'Conocenos', message)
-});
-
-
-bot.on('callback_query', (context) =>{
-    context.answerCbQuery()
-    
-    switch(context.update.callback_query.data){
-        case 'info':
-         context.reply('Asumo que aqui se consume Firebase y el servidor de Synergy');
-        break;
-
-        case 'service':
-        context.reply('Asumo que aqui se consume Firebase y el servidor de Synergy');
-        break;
-            
-        case 'mision':
-        context.reply('Asumo que aqui se consume Firebase y el servidor de Synergy');
-        break;
-        
-        case 'vision':
-        context.reply('Asumo que aqui se consume Firebase y el servidor de Synergy');
-        break;
-            
-        case 'joinus':
-        context.reply('Asumo que aqui se consume Firebase y el servidor de Synergy');
-        break;
-        
-        case 'visitus':
-        context.reply('Asumo que aqui se consume Firebase y el servidor de Synergy');
-        break;
-    }
-});
 
 
