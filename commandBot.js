58862f6da5df1881128a712d4fa267ea6c61710d
BEPZ22 <brauliopicon96@gmail.com>
2019-02-12 14:13:26 -0400

    Firestore Problemas

--- commandBot.js
+++ commandBot.js
@@ -6,12 +6,14 @@ module.exports = {
 
     GetStructure : function (coleccion,id){
         console.log(coleccion + id);
-        
-        var refUsers = db.Database().collection(coleccion).doc(id);
+        //collection.doc(doc.id).set(doc)
+        var refUsers = db.Database().collection(coleccion).doc(id).set(doc);
         var setUsers = refUsers.set({
         nombre: 'Synergy',
         apellido: 'Vision',
         email:'sinergyvision@',
+        }).catch(function (error){
+            console.log(error)
         });   
     }
 
