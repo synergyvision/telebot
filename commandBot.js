1b74d1efa153c48764da4cb9006dd898767afe2f
BEPZ22 <brauliopicon96@gmail.com>
2019-02-12 14:30:46 -0400

    Cambio de orden

--- commandBot.js
+++ commandBot.js
@@ -7,15 +7,14 @@ module.exports = {
     GetStructure : function (coleccion,id){
         console.log(coleccion + id);
         //collection.doc(doc.id).set(doc)
-        var refUsers = db.Database().collection('Usuario').doc(id);
+        var setUsers = {
+            nombre: 'Synergy',
+            apellido: 'Vision',
+            email:'sinergyvision@',
+            }
+        var refUsers = db.Database().collection('Usuario').doc(id).set(setUsers);
         console.log('pase el refUsers');
-        var setUsers = refUsers.set({
-        nombre: 'Synergy',
-        apellido: 'Vision',
-        email:'sinergyvision@',
-        }).catch(function (error){
-            console.log(error)
-        });   
+        
     }
 
 };
