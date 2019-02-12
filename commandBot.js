effa2454779bc69b61f0d91d597a58a6199f5347
BEPZ22 <brauliopicon96@gmail.com>
2019-02-12 14:27:52 -0400

    Error

--- commandBot.js
+++ commandBot.js
@@ -7,7 +7,7 @@ module.exports = {
     GetStructure : function (coleccion,id){
         console.log(coleccion + id);
         //collection.doc(doc.id).set(doc)
-        var refUsers = db.Database().collection(coleccion).doc(id);
+        var refUsers = db.Database().collection('Usuario').doc(id);
         console.log('pase el refUsers');
         var setUsers = refUsers.set({
         nombre: 'Synergy',
