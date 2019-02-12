48ef87396e6d052cdd999d4870bdc1bdac2ae882
BEPZ22 <brauliopicon96@gmail.com>
2019-02-12 12:18:39 -0400

    Descartando errores de codigo

--- commandBot.js
+++ commandBot.js
@@ -5,14 +5,13 @@ const db = require('./firebaseBot');
 module.exports = {
 
     GetStructure : function (coleccion,id){
-        var refUsers = db.Database().collection(coleccion).doc(id);
-
+        console.log(coleccion);
+        /*var refUsers = db.Database().collection(coleccion).doc(id);
         var setUsers = refUsers.set({
         nombre: 'Synergy',
         apellido: 'Vision',
         email:'sinergyvision@',
-        });
-
+        });*/    
     }
 
 };
