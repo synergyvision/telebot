0b06d40d30d4e3d62b1ba432aca25b33d6ed731e
BEPZ22 <brauliopicon96@gmail.com>
2019-02-12 17:31:43 -0400

    Funcion para agregar
    Agregacion de los usuarios

--- commandBot.js
+++ commandBot.js
@@ -5,15 +5,20 @@ const db = require('./firebaseBot');
 module.exports = {
 
     GetStructure : function (name,id,lastname){
-        console.log(name + id + lastname);
+        
 
+
+           
+    },
+
+    PostUsers : function(name,lastname,id,email){
         var setUsers = {
             nombre: name,
             apellido: id,
+            identification: id,
             email: lastname
         }
-
-        var refUsers = db.Database().collection('Usuario').doc().set(setUsers);   
-    }
-
+        var refUsers = db.Database().collection('Usuario').doc().set(setUsers);
+    },
+    
 };
