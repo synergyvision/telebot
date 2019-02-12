75b8fb26ac15d08aa24aeaf5c49b869fb3d19616
BEPZ22 <brauliopicon96@gmail.com>
2019-02-12 17:32:16 -0400

    Cambios

--- logicBot.js
+++ logicBot.js
@@ -55,7 +55,7 @@ bot.start((context)=>{
       
       switch(context.update.callback_query.data){
           case 'info':
-            command.GetStructure('Ramon','14132','Garanton');
+            command.GetStructure();
             context.reply('Asumo que aqui se consume Firebase y el servidor de Synergy');
           break;
   
