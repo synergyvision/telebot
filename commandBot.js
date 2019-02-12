ac75c51890ceb3a9abf89d6c8cc747949de04c52
BEPZ22 <brauliopicon96@gmail.com>
2019-02-12 11:54:29 -0400

    Probando BD
    problema en la agregacion del id resuelto

--- commandBot.js
+++ commandBot.js
@@ -5,13 +5,13 @@ const db = require('./firebaseBot');
 module.exports = {
 
     GetStructure : function (coleccion,id){
-        var refUsers = db.Database().collection(coleccion,id);
+        var refUsers = db.Database().collection(coleccion).doc(id);
 
-     var setUsers = refUsers.set({
-     nombre: 'Synergy',
-     apellido: 'Vision',
-     email:'sinergyvision@',
-     });
+        var setUsers = refUsers.set({
+        nombre: 'Synergy',
+        apellido: 'Vision',
+        email:'sinergyvision@',
+        });
 
     }
 
