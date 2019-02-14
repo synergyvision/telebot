877f0b763fa561989b30fbbe6f56ce6b7ad6daab
BEPZ22 <brauliopicon96@gmail.com>
2019-02-14 11:34:26 -0400

    Cambio de Nombre de una Clase
    Cambiando el nombre de la clase que ejecuta los comandos de los usuarios

--- commandUser.js
+++ commandUser.js
@@ -1,42 +1,36 @@
 const db = require('./firebaseBot');
 
-
-
 module.exports = {
 
-  //  GetStructure : function (collection,dato){
-  //      var refCollection = db.Database().collection(collection);
-  //      var queryCollection= refCollection.where('email','==',dato);
-       // var snapCollection = queryCollection.onSnapshot().;
-  //      return queryCollection;
-  //  },
-
     GetUsers : function (collectionID) {
+
         var refUsers = db.Database().collection('Usuario').doc(collectionID);
         
         return refUsers.get()
         .then(doc => {
                 if (!doc.exists) {
-                  console.log('No such document!');
+                  console.log('No existe el Usuario!');
                 } else {
                     var usuario = doc.data();
-                    //console.log('Document usuario:', usuario);
+                    //console.log('Documento usuario:', usuario);
                     return usuario;
                 }
               })
               .catch(err => {
-                console.log('Error getting document', err);
+                console.log('Error extrayendo al Usuario', err);
               });
         
     },
 
     PostUsers : function(name,lastname,id,email){
+      
         var setUsers = {
             nombre: name,
             apellido: lastname,
             identification: id,
             email: email
         }
+
         var refUsers = db.Database().collection('Usuario').doc(id).set(setUsers);
     },
     
